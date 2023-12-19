09c51b631e010abae5c4e65723b2d82d8c17a78b
odetv <abdiutama02@gmail.com>
2023-12-19 21:39:16 +0800

    fix all bugs and add emailJS feature

--- src/components/Informasi.tsx
+++ src/components/Informasi.tsx
@@ -1,4 +1,27 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 function Informasi() {
+    const form = useRef<HTMLFormElement | null>(null);
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        if (form.current) {
+            emailjs
+              .sendForm('service_eym8k1i', 'template_afo40a2', form.current, '6xddWmyE6VKHfojB3')
+              .then(
+                (result) => {
+                  console.log('Email berhasil terkirim!', result.status, result.text);
+                },
+                (error) => {
+                  console.error('Gagal mengirim email:', error.text);
+                }
+              );
+            form.current.reset();
+          }
+      };
+
     return (
       <section id="informasi" className="flex max-w-7xl mx-auto flex-col pb-20">
         <h1 className="text-center font-bold text-3xl mb-3">
@@ -10,20 +33,28 @@ function Informasi() {
         <section className="text-gray-600 body-font relative">
     <div className="container px-5 py-8 mx-auto flex sm:flex-nowrap flex-wrap">
         <div
-            className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-8 flex items-end justify-start relative">
             <iframe width="100%" height="100%" className="absolute inset-0" title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.865768967395!2d115.08732110000001!3d-8.1151472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd19b73c1ae2f6d%3A0xc50b17347a33b1e7!2sWashtop%20Laundry%201!5e0!3m2!1sid!2sid!4v1702894963328!5m2!1sid!2sid"
                 ></iframe>
             <div className="bg-white relative flex flex-wrap py-3 rounded shadow-2xl items-center mt-80 text-xs sm:text-sm md:text-sm lg:text-sm xl:text-sm">
                 <div className="lg:w-1/2 px-6">
                     <h2 className="title-font font-semibold text-gray-900">ALAMAT</h2>
-                    <p className="mt-1">Jl. Nusa Indah No.21, Kaliuntu, Kecamatan Buleleng, Kabupaten
-              Buleleng, Bali 81116</p>
+                    <a
+              className="mt-1 text-tiny sm:text-sm md:text-sm lg:text-sm xl:text-sm"
+              href="https://maps.app.goo.gl/hZ3m6FEWfknbRrBi6"
+              target="_blank"
+            >
+              Jl. Nusa Indah No.21, Kaliuntu, Kecamatan Buleleng, Kabupaten
+              Buleleng, Bali 81116
+            </a>
+              <h2 className="title-font font-semibold text-gray-900 mt-4">WHATSAPP</h2>
+                    <a href="https://wa.me/6282145555566/" target="_blank" className="leading-relaxed">082145555566</a>
                 </div>
                 <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                    <h2 className="title-font font-semibold text-gray-900">WHATSAPP</h2>
-                    <a href="https://wa.me/6282145555566/" target="_blank" className="leading-relaxed">082145555566</a>
-                    <h2 className="title-font font-semibold text-gray-900 mt-4">INSTAGRAM</h2>
+                    <h2 className="title-font font-semibold text-gray-900">INSTAGRAM</h2>
                     <a href="https://instagram.com/washtoplaundryexpress/" target="_blank" className="leading-relaxed">@washtoplaundryexpress</a>
+                    <h2 className="title-font font-semibold text-gray-900 mt-4">EMAIL</h2>
+                    <a href="mailto:washtoplaundryexpress@gmail.com" target="_blank" className="leading-relaxed">washtoplaundryexpress@gmail.com</a>
                 </div>
             </div>
         </div>
@@ -31,20 +62,22 @@ function Informasi() {
             <h2 className="text-gray-900 text-xl mb-2 font-bold">Punya Pertanyaan?</h2>
             <p className="leading-relaxed mb-5 text-gray-600">Kirim pertanyaan Anda melalui form di bawah ini.
             </p>
+            <form ref={form} onSubmit={sendEmail}>
             <div className="relative mb-4">
                 <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nama</label>
-                <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <input required type="text" id="name" name="user_name" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <div className="relative mb-4">
                 <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <input required type="email" id="email" name="user_email" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <div className="relative mb-4">
                 <label htmlFor="message" className="leading-7 text-sm text-gray-600">Pesan</label>
-                <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                <textarea required id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
-            <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded-lg transition-all ease-in-out font-medium">Kirim</button>
-        </div>
+            <button type="submit" className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded-lg transition-all ease-in-out font-medium">Kirim</button>
+            </form>
+            </div>
     </div>
 </section>
       </section>
--- src/components/Testimoni.tsx
+++ src/components/Testimoni.tsx
@@ -118,18 +118,16 @@ function Testimoni() {
                     </div>
 
                     <div className="mt-4">
-                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Stayin' Alive</p>
+                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Sinta P.</p>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms or legs … Where are
-                        they? You know what? If we come across somebody with no arms or legs, do we
-                        bother resuscitating them? I mean, what quality of life do we have there?
+                    Saya sangat puas dengan layanan Washtop Laundry Express! Kualitas cucian yang luar biasa dan pelayanan yang sangat ramah. Sangat membantu usaha saya dalam menjaga kualitas produk bersih dan rapi.
                     </p>
                     </div>
                 </div>
 
                 <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                    &mdash; Pengusaha UMKM
                 </footer>
                 </blockquote>
             </div>
@@ -198,18 +196,16 @@ function Testimoni() {
                     </div>
 
                     <div className="mt-4">
-                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Stayin' Alive</p>
+                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Andi</p>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms or legs … Where are
-                        they? You know what? If we come across somebody with no arms or legs, do we
-                        bother resuscitating them? I mean, what quality of life do we have there?
+                    Tempat cucian favorit saya sejak pertama kali mencoba! Cepat, efisien, dan hasilnya bersih sempurna. Cocok banget buat mahasiswa yang butuh kepraktisan!
                     </p>
                     </div>
                 </div>
 
                 <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                    &mdash; Mahasiswa
                 </footer>
                 </blockquote>
             </div>
@@ -278,18 +274,16 @@ function Testimoni() {
                     </div>
 
                     <div className="mt-4">
-                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Stayin' Alive</p>
+                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Rini</p>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms or legs … Where are
-                        they? You know what? If we come across somebody with no arms or legs, do we
-                        bother resuscitating them? I mean, what quality of life do we have there?
+                    Washtop Laundry Express adalah pilihan tepat untuk keluarga saya. Hasil cucian bersih, waktu pengerjaan yang singkat, dan harga yang terjangkau. Sungguh nyaman dan membantu!
                     </p>
                     </div>
                 </div>
 
                 <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                    &mdash; Ibu Rumah Tangga
                 </footer>
                 </blockquote>
             </div>
@@ -358,18 +352,94 @@ function Testimoni() {
                     </div>
 
                     <div className="mt-4">
-                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Stayin' Alive</p>
+                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Dewi Cintya</p>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                        No, Rose, they are not breathing. And they have no arms or legs … Where are
-                        they? You know what? If we come across somebody with no arms or legs, do we
-                        bother resuscitating them? I mean, what quality of life do we have there?
+                    Baru saja pindah ke Singaraja dan menemukan Washtop Laundry Express. Puas banget dengan kualitas dan layanannya! Saya sudah merasa 'at home' dengan layanan laundry yang nyaman ini.
                     </p>
                     </div>
                 </div>
 
                 <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-                    &mdash; Michael Scott
+                    &mdash; Pendatang Baru di Singaraja
+                </footer>
+                </blockquote>
+            </div>
+            </div>
+            <div id="testimoni5" className="text-center">
+            <div className="keen-slider__slide">
+                <blockquote
+                className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8 lg:p-12"
+                >
+                <div>
+                    <div className="flex gap-0.5 text-green-500 justify-center">
+                    <svg
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                        />
+                    </svg>
+
+                    <svg
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                        />
+                    </svg>
+
+                    <svg
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                        />
+                    </svg>
+
+                    <svg
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                        />
+                    </svg>
+
+                    <svg
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                        />
+                    </svg>
+                    </div>
+
+                    <div className="mt-4">
+                    <p className="text-2xl font-bold text-blue-600 sm:text-3xl">Putra Wiratama</p>
+
+                    <p className="mt-4 leading-relaxed text-gray-700">
+                    Sebagai pebisnis online, kebersihan barang dagangan sangat penting. Washtop Laundry Express memberikan solusi yang handal dengan hasil cucian yang selalu memuaskan. Terima kasih atas layanan yang konsisten!
+                    </p>
+                    </div>
+                </div>
+
+                <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
+                    &mdash; Pelaku Bisnis Online
                 </footer>
                 </blockquote>
             </div>
