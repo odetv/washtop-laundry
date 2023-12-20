960c8b1fdd0069475af540ea399528c15f4f02c6
odetv <abdiutama02@gmail.com>
2023-12-20 18:57:21 +0800

    enable config.ts

---
+++ config.ts
@@ -0,0 +1,8 @@
+const config = {
+    EMAILJS_API_KEY: "6xddWmyE6VKHfojB3",
+    EMAILJS_SERVICE_ID: "service_eym8k1i",
+    EMAILJS_TEMPLATE_ID: "template_afo40a2",
+  };
+  
+export default config;
+  
